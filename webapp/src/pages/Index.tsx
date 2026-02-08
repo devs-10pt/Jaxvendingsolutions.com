0c32a6ec98e5a422afcfc5ee5028e5807b2f58ad
Sandbox user <tools@notanumber.com>
2026-02-08 17:35:37 +0000

    nevermind remove both of these images

--- webapp/src/pages/Index.tsx
+++ webapp/src/pages/Index.tsx
@@ -3,7 +3,6 @@ import { Hero } from "@/components/sections/Hero";
 import { Process } from "@/components/sections/Process";
 import { SmartTechnology } from "@/components/sections/SmartTechnology";
 import { WhoWeServe } from "@/components/sections/WhoWeServe";
-import { MachineShowcase } from "@/components/sections/MachineShowcase";
 import { CTASection, Footer } from "@/components/sections/CTASection";
 
 const Index = () => {
@@ -14,7 +13,6 @@ const Index = () => {
       <Process />
       <WhoWeServe />
       <SmartTechnology />
-      <MachineShowcase />
       <CTASection />
       <Footer />
     </div>
